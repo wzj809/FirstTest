3f94942034a37b806d881dd9843035ea4480032b
lanou3g <553523042@qq.com>
2016-03-17 09:51:30 +0800

    第二次提交

---
+++ FirstsTest/FirstsTest/RootView.h
@@ -0,0 +1,13 @@
+//
+//  RootView.h
+//  FirstsTest
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 wzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootView : UIView
+
+@end
---
+++ FirstsTest/FirstsTest/RootView.m
@@ -0,0 +1,21 @@
+//
+//  RootView.m
+//  FirstsTest
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 wzj. All rights reserved.
+//
+
+#import "RootView.h"
+
+@implementation RootView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
